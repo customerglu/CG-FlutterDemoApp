74647221c623652b0260693059df346a0772eaf2
himCustomerGlu <88142817+himCustomerGlu@users.noreply.github.com>
2022-02-09 14:57:40 +0530

    Ui completed

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -3,6 +3,9 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:fluttersdkapp/shopScreen.dart';
+
+import 'cartScreen.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -13,87 +16,78 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height/2.8,
-                width: MediaQuery.of(context).size.width,
-                decoration:const  BoxDecoration(
-                    color: Colors.black
-                ),
-
-              ),
-              Image.asset("assets/images/customerglu.jpg")
-
-
-            ],
-          ),
-         const SizedBox(height: 30),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              boxIcon("assets/images/purse.png","Wallet",()=>
-              {
-                print("1")
-              }),
-              boxIcon("assets/images/quiz.png","Campaigns",()=>{
-                print("12")
-
-              }),
-            ],
-          ),
-         const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              boxIcon("assets/images/shop.png","Shop",()=>{
-                print("3")
-
-              }),
-              boxIcon("assets/images/trolley.png","Cart",()=>{
-                print("4")
-
-              }),
-            ],
-          )
-        ],
-      )
-    );
+        body: Column(
+      children: [
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 2.8,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(color: Colors.black),
+            ),
+            Image.asset("assets/images/customerglu.jpg")
+          ],
+        ),
+        const SizedBox(height: 30),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            boxIcon("assets/images/purse.png", "Wallet", () => {print("1")}),
+            boxIcon("assets/images/quiz.png", "Campaigns", () => {print("12")}),
+          ],
+        ),
+        const SizedBox(height: 30),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            boxIcon(
+                "assets/images/shop.png",
+                "Shop",
+                () => {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => ShopScreen()))
+                    }),
+            boxIcon(
+                "assets/images/trolley.png",
+                "Cart",
+                () => {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => CartScreen()))
+                    }),
+          ],
+        )
+      ],
+    ));
   }
-
-
 }
-Widget boxIcon(image,label,callback)
-{
+
+Widget boxIcon(image, label, callback) {
   return GestureDetector(
-    onTap: ()=>{ callback()} ,
+    onTap: () => {callback()},
     child: Card(
       elevation: 10,
-      child:Container(
+      child: Container(
         height: 150,
         width: 150,
-
         child: Stack(
           alignment: Alignment.center,
-
           children: [
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-
-                Image.asset(image,height: 80,width: 80,),
-                Text(label,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
-
+                Image.asset(
+                  image,
+                  height: 80,
+                  width: 80,
+                ),
+                Text(
+                  label,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                )
               ],
             )
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,57 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'homeScreen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyCO6T-C4hvJLE81DvjWEzNzpU_jOdZuEMc',
+      appId: '1:421104677220:android:42a6f9c16c6b8fa3808a71',
+      messagingSenderId: '421104677220',
+      projectId: 'modpod-e0ff6',
+    ),
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AppState();
+  }
+}
+
+class _AppState extends State<MyApp> {
+  var fcmtoken = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseMessaging.instance.getInitialMessage().then((message) async {});
+
+    var messaging = FirebaseMessaging.instance;
+
+    messaging.getToken().then((value) {
+      print(value);
+      fcmtoken = value!;
+      //Firebase Token
+    });
+
+    //  initializelocalNotification();
+
+    FirebaseMessaging.onMessage.listen((message) {});
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
