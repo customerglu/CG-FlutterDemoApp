cd096b09691ebad2660ecf7ce90628fd1367f502
Hitesh Landge <hitesh.landge@customerglu.net>
2022-03-08 15:45:44 +0530

    Done with iOS setup
    Done with iOS setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,74 @@
 import UIKit
 import Flutter
+import Firebase
+import CustomerGlu
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+
+let channelName = "CUSTOMERGLU_EVENTS"
+var methodChannel = FlutterMethodChannel()
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+
+NotificationCenter.default.addObserver(
+              self,
+              selector: #selector(self.catchDeeplinkNotification),
+              name: Notification.Name("CUSTOMERGLU_DEEPLINK_EVENT"),
+              object: nil)
+  NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.catchAnalyticsNotification),
+            name: Notification.Name("CUSTOMERGLU_ANALYTICS_EVENT"),
+            object: nil)
+
+
+
+    if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+    } else {
+        let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+    }
+
+    application.registerForRemoteNotifications()
+
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+
+override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                     willPresent notification: UNNotification,
+                                     withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    CustomerGlu.getInstance.cgUserNotificationCenter(center, willPresent: notification, withCompletionHandler: completionHandler)
+}
+
+@objc private func catchAnalyticsNotification(notification: NSNotification) {
+    if let userInfo = notification.userInfo as? [String: Any]
+    {
+        print(userInfo)
+        methodChannel.invokeMethod("CUSTOMERGLU_ANALYTICS_EVENT", arguments: userInfo)
+    }
+}
+
+@objc private func catchDeeplinkNotification(notification: NSNotification) {
+    //do stuff using the userInfo property of the notification object
+    if let userInfo = notification.userInfo as? [String: Any]
+    {
+        print(userInfo)
+        methodChannel.invokeMethod("CUSTOMERGLU_DEEPLINK_EVENT", arguments: userInfo)
+    }
+} 
 }
